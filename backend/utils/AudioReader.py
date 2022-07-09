10415cd49f9e337eed92799064be7485f2804d34
lovemefan <450489712@qq.com>
2022-07-09 16:03:28 +0800

    add python doc

--- backend/utils/AudioReader.py
+++ backend/utils/AudioReader.py
@@ -10,9 +10,14 @@ import soundfile as sf
 import numpy as np
 
 class AudioReader:
+    """
+
+    read audio from sanic request
+    """
     def __init__(self):
         pass
 
+    @staticmethod
     def get_info(self, path: str):
         with open(path, 'rb') as f:
             name, data_lengths, _, _, _, _, channels, sample_rate, bit_rate, block_length, sample_bit, _, pcm_length = struct.unpack_from(
@@ -23,11 +28,18 @@ class AudioReader:
 
     @staticmethod
     def read_pcm16(data: bytes):
+        """
+        convert bytes into array of pcm_s16le data
+        :param data: PCM format bytes
+        :return:
+        """
         shortArray = array.array('h')  # int16
+        # header of wav file
         info = data[:44]
         frames = data[44:]
         name, data_lengths, _, _, _, _, channels, sample_rate, bit_rate, block_length, sample_bit, _, pcm_length = struct.unpack_from(
             '<4sL4s4sLHHLLHH4sL', info)
+        # shortArray each element is 16bit
         shortArray.frombytes(frames)  # struct.unpack
         data = np.array(shortArray)
         # 缩放
