f1bf4612caba883a2654af8b4d4c5614b264d18a
lovemefan <450489712@qq.com>
2022-07-09 16:59:16 +0800

    add run script

--- backend/scripts/run-dev.sh
+++ backend/scripts/run-dev.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 set -o errexit
 
-app="/SanicExample/backend"
-venv="${app}/venv"
+apt-get update -y && apt-get install libsndfile1 -y
+
+app="../"
 
 echo "Installing dependencies"
 python3 -m pip install --upgrade pip
 pip3 install -r "${app}/requirements.txt" -i "https://pypi.tuna.tsinghua.edu.cn/simple"
-
+pip3 install -e "${app}../fairseq_lib"
 echo "Starting Sanic development server"
-python3 "${app}/routes/app.py"
+cd $app
+python3 "routes/app.py"
